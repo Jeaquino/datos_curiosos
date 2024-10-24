e525d1d4f7c0d3cbf2cdd5ba8d356c9e8a712bfc
julian E. Aquino <julian.aquino@cencosud.com.ar>
2024-10-24 16:50:43 -0300

    add prompt module

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const saludo = require('./modules/saludo');
 const despedida = require('./modules/despedida');
 const brindarDatoCurioso = require('./modules/datoCurioso');
-const nombre = process.argv[2]
+// const nombre = process.argv[2]
 
+var prompt = require('prompt-sync')({
+    sigint: true
+  });
+
+const nombre = prompt("Dime cual es tu nombre?");
 saludo(nombre);
 console.log("Te comparto un dato curioso:");
 brindarDatoCurioso();
